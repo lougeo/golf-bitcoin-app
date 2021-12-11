7bd46f59b521de348e27bdedc1d2edfc91a9c8f6
Louis George <louisg495@gmail.com>
2021-12-11 14:14:59 -0800

    pass data to course detail

--- app/screens/CourseList.js
+++ app/screens/CourseList.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Button, Text, FlatList, TouchableOpacity } from "react-native";
+import { StyleSheet, View, TextInput, Text, FlatList, TouchableOpacity } from "react-native";
+import { SearchBar } from "react-native-elements";
 import { AuthContext } from '../providers/AuthContext';
 import { base_url } from "../Globals";
 
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-    <Text style={[styles.title, textColor]}>{item.name}</Text>
-  </TouchableOpacity>
-);
 
 function CourseListScreen({ navigation }) {
   const { userToken } = React.useContext(AuthContext);
@@ -26,42 +22,94 @@ function CourseListScreen({ navigation }) {
     })
       .then(response => response.json())
       .then(json => {
-        console.log(json);
         setCourses(json);
       })
       .catch((error) => {
         // Network error, server off or similar.
-        console.error("Error:", error);
+        console.error(error);
       });
   }, []);
 
   return (
     <SafeAreaView>
-      <Text>COURSE LIST</Text>
+      
+      <SearchBar
+        lightTheme="true"
+        // style={styles.inputView}
+        placeholder="Search"
+        // value={email}
+        // onChangeText={setEmail}
+      />
+
       <FlatList
         data={courses}
-        renderItem={Item}
-        onPress={() => setSelectedId(item.id)}
+        // onPress={() => navigation.navigate("CourseDetail")}
         renderItem={({ item, onPress, backgroundColor, textColor }) => (
-          <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+          <TouchableOpacity onPress={() => navigation.navigate("CourseDetail", {id: item.id})} style={[styles.item, backgroundColor]}>
             <Text style={[styles.title, textColor]}>{item.name}</Text>
           </TouchableOpacity>
         )}
       />
-      <Button title="Add Course" />
+
+      <TouchableOpacity style={styles.addBtn}>
+        <Text style={styles.actionBtnText}>ADD</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
-export default CourseListScreen;
-
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   item: {
+    backgroundColor: "#9db802",
+    borderRadius: 30,
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
   },
+
   title: {
     fontSize: 32,
   },
+
+  inputView: {
+    backgroundColor: "#9db802",
+    borderRadius: 30,
+    width: "100%",
+    height: 45,
+    marginTop: 15,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+
+  textInput: {
+    color: "white",
+    flex: 1,
+  },
+ 
+  addBtn: {
+    alignSelf: "flex-end",
+    position: "absolute",
+    bottom: 0,
+    right: 10,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#025b0e",
+  },
+
+  actionBtnText: {
+    color: "white",
+  }
 });
+
+
+export default CourseListScreen;
