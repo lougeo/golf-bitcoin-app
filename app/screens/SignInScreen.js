ab643f0ac2c13e084352e7e734837efb72808855
Louis George <louisg495@gmail.com>
2021-12-11 10:37:10 -0800

    styling

--- app/screens/SignInScreen.js
+++ app/screens/SignInScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Button, TextInput } from "react-native";
+import { StyleSheet, TouchableOpacity, TextInput, View, Text } from "react-native";
 import { AuthContext } from '../providers/AuthContext';
 
 
@@ -12,33 +12,81 @@ function SignInScreen({ navigation }) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign in" onPress={() => signIn({ email, password })} />
-      <Button title="Register" onPress={() => navigation.navigate("Register")} />
+
+      <View style={styles.inputView}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.textInput}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          style={styles.textInput}
+          secureTextEntry
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.resetBtn}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.actionBtn} onPress={() => signIn({ email, password })}>
+        <Text style={styles.actionBtnText}>LOGIN</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.actionBtn} onPress={() => navigation.navigate("Register")}>
+        <Text style={styles.actionBtnText}>REGISTER</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        backgroundColor: "#F0EAD6",
-    },
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  inputView: {
+    backgroundColor: "#9db802",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+
+  textInput: {
+    color: "white",
+    flex: 1,
+  },
+ 
+  resetBtn: {
+    height: 30,
+    marginBottom: 30,
+  },
+ 
+  actionBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#025b0e",
+  },
+
+  actionBtnText: {
+    color: "white",
+  }
 });
 
 export default SignInScreen;
