4502eb8764816202c94b4a3fc212229551aae2f1
Louis George <louisg495@gmail.com>
2021-12-12 13:47:31 -0800

    add settings screen

---
+++ app/screens/SettingsScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Button, Text, View, TouchableOpacity } from "react-native";
+import { AuthContext } from '../providers/AuthContext';
+
+
+function SettingsScreen({ navigation }) {
+  const { signOut } = React.useContext(AuthContext);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Settings</Text>
+
+      <TouchableOpacity style={[styles.actionBtn, {backgroundColor: "#cfcdcb"}]} onPress={signOut}>
+        <Text style={[styles.actionBtnText, {color: "black"}]}>SIGN OUT</Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ 
+  actionBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#025b0e",
+  },
+
+  actionBtnText: {
+    color: "white",
+  }
+});
+
+export default SettingsScreen;
