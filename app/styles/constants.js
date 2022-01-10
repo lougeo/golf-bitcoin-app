71923b26297b2e030218b1ace17ab1e26f3fe350
Louis George <louisg495@gmail.com>
2022-01-09 21:31:05 -0800

    global styles

---
+++ app/styles/constants.js
@@ -0,0 +1,3 @@
+
+export const primaryColor = "#025b0e";
+export const secondaryColor = "#9db802";
---
+++ app/styles/mainStyles.js
@@ -0,0 +1,74 @@
+import { StyleSheet } from "react-native";
+import { primaryColor, secondaryColor } from "./constants";
+
+
+const mainStyles = StyleSheet.create({
+
+  flcontainer: {
+    flex: 1,
+  },
+
+  containerCenter: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  listItem: {
+    backgroundColor: secondaryColor,
+    borderRadius: 30,
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+
+  title: {
+    fontSize: 32,
+  },
+
+  inputView: {
+    backgroundColor: secondaryColor,
+    borderRadius: 30,
+    width: "100%",
+    height: 45,
+    marginTop: 15,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+
+  textInput: {
+    color: "white",
+    flex: 1,
+  },
+ 
+  addBtn: {
+    alignSelf: "flex-end",
+    position: "absolute",
+    bottom: 0,
+    right: 10,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: primaryColor,
+  },
+
+  actionBtnText: {
+    color: "white",
+  },
+ 
+  actionBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: primaryColor,
+  },
+  
+});
+
+export { mainStyles }
